fecc4a6d1e29ab8394f5765405444b4dfa19e5b1
thiennd <thiennd@edsolabs.com>
2025-04-16 22:34:01 +0700

    Update remove org struct

--- src/Aqt.CoreFW.Application.Contracts/Permissions/CoreFWPermissionDefinitionProvider.cs
+++ src/Aqt.CoreFW.Application.Contracts/Permissions/CoreFWPermissionDefinitionProvider.cs
@@ -11,32 +11,11 @@ public class CoreFWPermissionDefinitionProvider : PermissionDefinitionProvider
     {
         var myGroup = context.AddGroup(CoreFWPermissions.GroupName, L("Permission:CoreFWGroup"));
 
-        //Define your own permissions here. Example:
-        //myGroup.AddPermission(CoreFWPermissions.MyPermission1, L("Permission:MyPermission1"));
-
         // Define permissions for Countries
         var countriesPermission = myGroup.AddPermission(CoreFWPermissions.Countries.Default, L("Permission:Countries"));
         countriesPermission.AddChild(CoreFWPermissions.Countries.Create, L("Permission:Countries.Create"));
         countriesPermission.AddChild(CoreFWPermissions.Countries.Edit, L("Permission:Countries.Edit"));
         countriesPermission.AddChild(CoreFWPermissions.Countries.Delete, L("Permission:Countries.Delete"));
-
-        // Định nghĩa quyền cho JobTitles (Sử dụng hằng số từ CoreFWPermissions)
-        var jobTitlesPermission = myGroup.AddPermission(CoreFWPermissions.JobTitles.Default, L("Permission:JobTitles")); // Key localization mới
-        jobTitlesPermission.AddChild(CoreFWPermissions.JobTitles.Create, L("Permission:JobTitles.Create"));       // Key localization mới
-        jobTitlesPermission.AddChild(CoreFWPermissions.JobTitles.Edit, L("Permission:JobTitles.Edit"));           // Key localization mới
-        jobTitlesPermission.AddChild(CoreFWPermissions.JobTitles.Delete, L("Permission:JobTitles.Delete"));       // Key localization mới
-
-        // Định nghĩa quyền cho UserProfiles (Sử dụng hằng số từ CoreFWPermissions)
-        var userProfilesPermission = myGroup.AddPermission(CoreFWPermissions.UserProfiles.Default, L("Permission:UserProfiles")); // Key localization mới
-        userProfilesPermission.AddChild(CoreFWPermissions.UserProfiles.Edit, L("Permission:UserProfiles.Edit")); // Key localization mới
-
-        // Định nghĩa quyền cho Positions (Sử dụng hằng số từ CoreFWPermissions)
-        var positionsPermission = myGroup.AddPermission(CoreFWPermissions.Positions.Default, L("Permission:Positions")); // Key localization mới
-        positionsPermission.AddChild(CoreFWPermissions.Positions.Create, L("Permission:Positions.Create"));        // Key localization mới
-        positionsPermission.AddChild(CoreFWPermissions.Positions.Edit, L("Permission:Positions.Edit"));          // Key localization mới
-        positionsPermission.AddChild(CoreFWPermissions.Positions.Delete, L("Permission:Positions.Delete"));        // Key localization mới
-        positionsPermission.AddChild(CoreFWPermissions.Positions.AssignRoles, L("Permission:Positions.AssignRoles")); // Key localization mới
-        positionsPermission.AddChild(CoreFWPermissions.Positions.SetAsPrimary, L("Permission:Positions.SetAsPrimary")); // Key localization mới
     }
 
     private static LocalizableString L(string name)
--- src/Aqt.CoreFW.Domain.Shared/CoreFWModuleExtensionConfigurator.cs
+++ src/Aqt.CoreFW.Domain.Shared/CoreFWModuleExtensionConfigurator.cs
@@ -1,10 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Volo.Abp.Identity;
-using Volo.Abp.ObjectExtending;
-using Volo.Abp.Threading;
-using Volo.Abp.Localization; // Cần cho LocalizableString
-using Aqt.CoreFW.Localization;
-using Aqt.CoreFW.Domain.Shared.OrgStructure; // Namespace chứa CoreFWResource (sẽ tạo sau)
+﻿using Volo.Abp.Threading;
 
 namespace Aqt.CoreFW;
 
@@ -70,33 +64,5 @@ public static class CoreFWModuleExtensionConfigurator
          * See the documentation for more:
          * https://docs.abp.io/en/abp/latest/Module-Entity-Extensions
          */
-
-        ObjectExtensionManager.Instance.Modules()
-                .ConfigureIdentity(identity => // Nhắm vào module Identity
-                {
-                    identity.ConfigureOrganizationUnit(ou => // Nhắm vào entity OrganizationUnit
-                    {
-                        // Thêm thuộc tính InteroperabilityCode
-                        ou.AddOrUpdateProperty<string>(
-                            OrgStructureConsts.OuExtensionPropertyInteroperabilityCode, // Tên thuộc tính (sẽ tạo const sau)
-                            property =>
-                            {
-                                // Thêm cấu hình nếu cần (validation, UI hints, etc.)
-                                property.DisplayName = LocalizableString.Create<CoreFWResource>(OrgStructureConsts.OuExtensionPropertyInteroperabilityCode); // Có thể thêm DisplayName tương tự nếu cần
-                                property.Attributes.Add(new StringLengthAttribute(OrgStructureConsts.MaxInteroperabilityCodeLength));
-                            }
-                        );
-
-                        // Thêm thuộc tính Address
-                        ou.AddOrUpdateProperty<string>(
-                            OrgStructureConsts.OuExtensionPropertyAddress, // Tên thuộc tính (sẽ tạo const sau)
-                            property =>
-                            {
-                                property.DisplayName = LocalizableString.Create<CoreFWResource>(OrgStructureConsts.OuExtensionPropertyAddress); // Sử dụng tên hằng số làm localization key luôn
-                                property.Attributes.Add(new StringLengthAttribute(OrgStructureConsts.MaxAddressLength));
-                            }
-                        );
-                    });
-                });
     }
 }
